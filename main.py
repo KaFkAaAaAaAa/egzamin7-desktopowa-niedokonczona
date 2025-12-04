7e37fc9128aea7d6f5b58d526b39bce3cc4ee926
Student <student@student>
2025-12-04 11:20:57 +0100

    komit

--- main.py
+++ main.py
@@ -21,9 +21,34 @@ class My_Dialog(QDialog):
 
 
     def okButton(self):
-        if self.ui.userNumber == "333":
-            pixmap = QPixmap("./333-zdjecie.jpg")
-            self.ui.imageLeft.setPixmap("./333-zdjecie.jpg")
+        name = self.ui.userName.toPlainText()
+        surname = self.ui.userSurname.toPlainText()
+        number = self.ui.userNumber.toPlainText()
+        eye_color = ""
+
+        if number == "333":
+            pixmap = QPixmap('./333-zdjecie.jpg')
+            pixmap1 = QPixmap('./333-odcisk.jpg')
+            self.ui.imageLeft.setPixmap(pixmap)
+            self.ui.imageRight.setPixmap(pixmap1)
+        elif number == "111":
+            pixmap2 = QPixmap('./111-zdjecie.jpg')
+            self.ui.imageLeft.setPixmap(pixmap2)
+
+        if self.ui.blueEye.isChecked():
+            eye_color = "niebieskie"
+        elif self.ui.brownEye.isChecked():
+            eye_color = "piwne"
+        else:
+            eye_color = "zielone"
+
+        if name == "" or surname == "" or number == "":
+            QMessageBox.information(self, "Info", "Uzupełnij wszystkie pola!")
+        else:
+            QMessageBox.information(self, "Info", f"{name} {surname} kolor oczu {eye_color}")
+
+
+
 
 
 
